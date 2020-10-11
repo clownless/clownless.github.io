e761ddf62ba57640ced102a678cb8c55c7045360
m4x3r <m4x3r1337@yandex.ru>
2020-10-11 20:07:45 +0400

    ?

---
+++ assets/js/typer.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(".typed").typed({
+        strings: ["who i am?", "who are you?", "why did you come here?", "what do you want here?"],
+        typeSpeed: 100,
+        loop: true,
+        cursorChar: "|",
+    });
+});
+        
